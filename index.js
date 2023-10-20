53e26110a1497bff6cbcae5c2e0b52e3f12ef24e
forme008 <estellaxabibullaeva0308@gmail.com>
2023-10-20 13:19:57 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,98 @@
+// // sinf ishi 1
+
+// function but1Func() {
+//     document.body.style.backgroundColor = "green"
+// }
+
+// function but2Func() {
+//     document.body.style.backgroundColor = "orange"
+// }
+
+// function but3Func() {
+//     document.body.style.backgroundColor = "black"
+// }
+
+// function but4Func() {
+//     document.body.style.backgroundColor = "pink"
+// }
+
+// function but5Func() {
+//     document.body.style.backgroundColor = "purple"
+// }
+
+
+// // sinf ishi 2
+
+// let fontSFunc = 16
+// const but6Func = function () {
+//     fontSFunc = fontSFunc + 2
+//     document.body.style.fontSize = fontSFunc + "px"
+// }
+
+// let fontFunc = 16
+// const but7Func = function () {
+//     fontFunc = fontFunc - 2
+//     document.body.style.fontSize = fontFunc + "px"
+// }
+
+// // sinf ishi 3
+
+// function colorFunc(){
+//     let color = prompt("rangni kriting")
+//     document.body.style = `background-color: ${color}`
+// }
+
+
+
+// // Homeworks
+
+// // firs task
+// let fontSFunc = 16
+// const plural = function () {
+//     fontSFunc = fontSFunc + 2
+//     document.body.style.fontSize = fontSFunc + "px"
+// }
+
+// // second task 
+
+// let fontFunc = 16
+// const singular = function () {
+//     fontFunc = fontFunc - 2
+//     document.body.style.fontSize = fontFunc + "px"
+// }
+
+// // third task
+
+// function day() {
+//     document.body.style.backgroundColor = "black"
+// }
+
+// function night() {
+//     document.body.style.backgroundColor = "white"
+// }
+
+// Forth task
+
+// function RedNOranche(){
+//     document.body.style.background = "red" 
+//     document.body.style.color = "orange" 
+
+// }
+
+// fifth
+
+const me = function () {
+   
+    document.body.style.fontSize = "25px"
+}
+ // sixth
+
+ const you = function() {
+    document.body.style.fontSize = "12px"
+ } 
+
+ //seventh
+
+ const we = function() {
+    document.body.style.fontSize = "16px"
+ }
